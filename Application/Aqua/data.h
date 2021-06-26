cc34339f3ec76a50e32219fd0909793e2252f1f3
SVBoyadzhieva18 <SVBoyadzhieva18@codingburgas.bg>
2021-06-26 22:59:48 +0300

    Fix some errors

---
+++ Application/Aqua/data.h
@@ -0,0 +1,29 @@
+#ifndef DATA_H
+#define DATA_H
+#include <string>
+using namespace std;
+
+struct DATE
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct DOLPHINS
+{
+	string seaStation;
+	string dolphinType;
+	int chipNumber;
+};
+
+void sortAlphabeticallySeaStations(DOLPHINS* dolphin, DATE* date, int& dolphinIndex);
+void sortAlphabeticallyDolphinType(DOLPHINS* dolphin, DATE* date, int& dolphinIndex);
+void searchBySeaStation(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void addDolphinType(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void addSeaStation(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void addDolphin(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void searchByDolphinType(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void enterDate(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+void searchByChipNumber(DOLPHINS* dolphin, DATE* date, int& dolphinsIndex);
+#endif // !DATA_H
